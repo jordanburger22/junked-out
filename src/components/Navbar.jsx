31b457750ede36030ddf7d14b0df5dc63f988fd8
Jordan Burger <jordanburger22@gmail.com>
2024-08-27 03:14:39 +0000

    added navigation to logo in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import logo from '../assets/BlueLogo.webp'
 import NavMenu from './NavMenu';
-
+import {useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
+
+    const navigate = useNavigate()
+
     return (
         <nav className='navbar'>
-            <img className='navbar-logo' src={logo} />
+            <img className='navbar-logo' src={logo} onClick={() => navigate('/')}/>
             <div className='navbar-btns'>
                 <NavMenu />
             </div>
